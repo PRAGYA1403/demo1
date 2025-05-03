2dd53eb9beb06f7a951e3d89818763f0225e0ebc
PRAGYA SHRIVASTAVA <119096125+PRAGYA1403@users.noreply.github.com>
2025-05-03 21:10:54 +0530

    Delete CAPSTONES directory

--- CAPSTONES/2141019301_LOS_Capstone-5/log_monitor.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-LOG_FILE="/var/log/syslog"
-KEYWORDS=("error" "fail" "panic")
-
-echo "Monitoring logs for keywords..."
-for keyword in "${KEYWORDS[@]}"; do
-    grep -i "$keyword" "$LOG_FILE" && echo "Keyword '$keyword' found!" 
-done
